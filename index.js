e8014d37cec1a4951c2890bb6ca44e2432c1ea28
jaekook <jaekook4849@gmail.com>
2020-12-16 22:16:21 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jaekook:wornr@1994@boilerplate.ovxc1.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -12,9 +24,23 @@ mongoose.connect('mongodb+srv://jaekook:wornr@1994@boilerplate.ovxc1.mongodb.net
   .catch(error => console.log(error));
 
 
-
 app.get('/', (request, response) => {
-    response.send('안녕하세요!');
+    response.send('안녕하세요! 이재국입니다 :)');
+});
+
+app.post('/register', (request, response) => {
+    /*
+        회원가입할 때 필요한 정보들을 client에서 가져오면
+        그것들을 데이터베이스에 넣어준다.
+    */
+    const user = new User(request.body);
+
+    user.save((err, userInfo) => {
+        if(err) return response.json({ success: false, err });
+        return response.status(200).json({
+            success: true
+        });
+    });
 });
 
 app.listen(port, () => {
